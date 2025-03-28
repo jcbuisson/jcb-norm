f6823c151aa0a63647667994d074d619d1a740d4
JC BUISSON <buisson@enseeiht.fr>
2025-03-28 07:57:13 +0100

    WIP

--- src/jcb-norm.js
+++ src/jcb-norm.js
@@ -29,12 +29,6 @@ export class Norm extends LitElement {
       return ((value - this.domain.inf) / (this.domain.sup - this.domain.inf) * 100.).toPrecision(3)
    }
 
-   // Percentage value for norm.inf
-   // domain.inf -> 0%, domain.sup -> 100%
-   get normInfPercentage() {
-      return this.valueToPercentage(this.norm.inf)
-   }
-
    get normInf() {
       return this.norm.inf.toPrecision(3)
    }
@@ -42,11 +36,17 @@ export class Norm extends LitElement {
    get normSup() {
       return this.norm.sup.toPrecision(3)
    }
+
+   // Percentage value for norm.inf
+   // domain.inf -> 0%, domain.sup -> 100%
+   get normInfPercentage() {
+      return this.valueToPercentage(this.norm.inf)
+   }
    
    // Percentage value for norm.sup
    // domain.inf -> 0%, domain.sup -> 100%
    get normSupPercentage() {
-      return ((this.norm.sup - this.domain.inf) / (this.domain.sup - this.domain.inf) * 100.).toPrecision(3)
+      return this.valueToPercentage(this.norm.sup)
    }
 
    get normTypicalPercentage() {
@@ -57,12 +57,19 @@ export class Norm extends LitElement {
       console.log('domain', this.domain)
       console.log('norm', this.norm)
       return html`
+         <!-- bar with color gradients -->
          <div style="height: 50%; position: relative; top: 25%; background: linear-gradient(to right, ${this.colorInf} 0%, ${this.colorNormal} ${this.normInfPercentage}%, ${this.colorNormal} ${this.normSupPercentage}%, ${this.colorSup} 100%);">
+            <!-- inf norm text -->
             <span style="position: relative; top: -50%; left: ${this.normInfPercentage}%; color: ${this.colorInf};">${this.normInf}</span>
+            <!-- typical norm text -->
             ${this.norm.typical
                ? html`<span style="position: relative; top: -50%; left: ${this.normTypicalPercentage}%; color: ${this.colorNormal};">${this.normTypicalPercentage}</span>`
                : ''}
+            <!-- sup norm text -->
             <span style="position: relative; top: -50%; left: ${this.normSupPercentage}%; color: ${this.colorSup};">${this.normSup}</span>
+
+            <!-- value triangle -->
+            <polygon points="50 15, 100 100, 0 100"/>
          </div>
       `
    }
