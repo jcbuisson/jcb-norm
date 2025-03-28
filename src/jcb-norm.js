6e30a2f818a90a3c164ed35079217dd7d629641d
JC BUISSON <buisson@enseeiht.fr>
2025-03-28 07:22:26 +0100

    WIP

--- src/jcb-norm.js
+++ src/jcb-norm.js
@@ -17,27 +17,32 @@ export class Norm extends LitElement {
       super()
       // default values - before override by attributes
       this.domain = { inf: 0., sup: 100. }
-      this.norm = { d1: 40., d2: 60., dt1: 5., dt2: 5., h: 0. }
+      this.norm = { inf: 40., sup: 60. }
       this.value = 50.
       this.colorLow = '#51AEE9'
       this.colorNormal = '#9FCE65'
       this.colorHigh = '#F6C242'
    }
    
-   get lowPercentage() {
-      return (this.norm.inf - this.domain.inf) / (this.domain.sup - this.domain.inf) * 100.
+   // Percentage value for norm.inf
+   // domain.inf -> 0%, domain.sup -> 100%
+   get normInfPercentage() {
+      return ((this.norm.inf - this.domain.inf) / (this.domain.sup - this.domain.inf) * 100.).toPrecision(3)
    }
    
-   get highPercentage() {
-      return (this.norm.sup - this.domain.inf) / (this.domain.sup - this.domain.inf) * 100.
+   // Percentage value for norm.sup
+   // domain.inf -> 0%, domain.sup -> 100%
+   get normSupPercentage() {
+      return ((this.norm.sup - this.domain.inf) / (this.domain.sup - this.domain.inf) * 100.).toPrecision(3)
    }
 
    render() {
+      console.log('domain', this.domain)
+      console.log('norm', this.norm)
       return html`
-         <div style="background: linear-gradient(to right, ${this.colorLow} 0%, ${this.colorNormal} ${this.lowPercentage.toPrecision(3)}%, ${this.colorNormal} ${this.highPercentage.toPrecision(3)}%, ${this.colorHigh} 100%);">
-            <div id="myBar"></div>
-            <div style="position: absolute; left: ${this.highPercentage}%">${this.highPercentage}</div>
-            
+         <div style="background: linear-gradient(to right, ${this.colorLow} 0%, ${this.colorNormal} ${this.normInfPercentage}%, ${this.colorNormal} ${this.normSupPercentage}%, ${this.colorHigh} 100%);">
+            <span style="position: relative; left: ${this.normInfPercentage}%;">${this.normInfPercentage}</span>
+            <span style="position: relative; left: ${this.normSupPercentage}%;">${this.normSupPercentage}</span>
          </div>
       `
    }
@@ -49,10 +54,7 @@ export class Norm extends LitElement {
             display: inline-block; /* by default a CE is inline and width & height do not apply */
             width: 100%; /* <jcb-norm> takes full parent width */
             height: 100%; /* <jcb-norm> takes full parent height */
-         }
-         
-         #myBar {
-            height: 10px;
+            font-family: var(--jcb-norm-font-family, Arial);
          }
       `
    }
