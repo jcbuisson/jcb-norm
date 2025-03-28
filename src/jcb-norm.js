9e817c629481613b58b6576ba00109b8f7ade9b6
JC BUISSON <buisson@enseeiht.fr>
2025-03-28 07:50:43 +0100

    WIP

--- src/jcb-norm.js
+++ src/jcb-norm.js
@@ -7,9 +7,9 @@ export class Norm extends LitElement {
          domain: {type: Object},
          norm: { type: Object },
          value: { type: Number },
-         colorNormInf: { type: String },
+         colorInf: { type: String },
          colorNormal: { type: String },
-         colorNormSup: { type: String },
+         colorSup: { type: String },
       }
    }
 
@@ -19,15 +19,28 @@ export class Norm extends LitElement {
       this.domain = { inf: 0., sup: 100. }
       this.norm = { inf: 40., sup: 60. }
       this.value = 50.
-      this.colorNormInf = '#51AEE9'
+      this.colorInf = '#51AEE9'
       this.colorNormal = '#9FCE65'
-      this.colorNormSup = '#F6C242'
+      this.colorSup = '#F6C242'
    }
-   
+      
+   // domain.inf -> 0%, domain.sup -> 100%
+   valueToPercentage(value) {
+      return ((value - this.domain.inf) / (this.domain.sup - this.domain.inf) * 100.).toPrecision(3)
+   }
+
    // Percentage value for norm.inf
    // domain.inf -> 0%, domain.sup -> 100%
    get normInfPercentage() {
-      return ((this.norm.inf - this.domain.inf) / (this.domain.sup - this.domain.inf) * 100.).toPrecision(3)
+      return this.valueToPercentage(this.norm.inf)
+   }
+
+   get normInf() {
+      return this.norm.inf.toPrecision(3)
+   }
+
+   get normSup() {
+      return this.norm.sup.toPrecision(3)
    }
    
    // Percentage value for norm.sup
@@ -36,13 +49,20 @@ export class Norm extends LitElement {
       return ((this.norm.sup - this.domain.inf) / (this.domain.sup - this.domain.inf) * 100.).toPrecision(3)
    }
 
+   get normTypicalPercentage() {
+      return this.valueToPercentage(this.norm.typical)
+   }
+
    render() {
       console.log('domain', this.domain)
       console.log('norm', this.norm)
       return html`
-         <div style="height: 50%; position: relative; top: 25%; background: linear-gradient(to right, ${this.colorNormInf} 0%, ${this.colorNormal} ${this.normInfPercentage}%, ${this.colorNormal} ${this.normSupPercentage}%, ${this.colorNormSup} 100%);">
-            <span style="position: relative; top: -50%; left: ${this.normInfPercentage}%; color: ${this.colorNormInf};">${this.normInfPercentage}</span>
-            <span style="position: relative; top: -50%; left: ${this.normSupPercentage}%; color: ${this.colorNormSup};">${this.normSupPercentage}</span>
+         <div style="height: 50%; position: relative; top: 25%; background: linear-gradient(to right, ${this.colorInf} 0%, ${this.colorNormal} ${this.normInfPercentage}%, ${this.colorNormal} ${this.normSupPercentage}%, ${this.colorSup} 100%);">
+            <span style="position: relative; top: -50%; left: ${this.normInfPercentage}%; color: ${this.colorInf};">${this.normInf}</span>
+            ${this.norm.typical
+               ? html`<span style="position: relative; top: -50%; left: ${this.normTypicalPercentage}%; color: ${this.colorNormal};">${this.normTypicalPercentage}</span>`
+               : ''}
+            <span style="position: relative; top: -50%; left: ${this.normSupPercentage}%; color: ${this.colorSup};">${this.normSup}</span>
          </div>
       `
    }
